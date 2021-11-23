77810f7f88667d13970eb4f4e6507d8482d05d0f
fareedsuri <fareedsuri@gmail.com>
2021-11-22 18:21:48 -0600

    remove unnecessary call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('client side java file')
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
